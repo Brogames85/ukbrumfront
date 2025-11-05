e11812cb09827c06131c708b97cbbd919c57f481
Mahir Handa <78787711+Brogames85@users.noreply.github.com>
2025-11-05 15:37:56 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>UKBRUM IS LIVE 🔥🔥</h1>
+      <p>Frontend deployed successfully!</p>
+    </div>
+  );
+}
+
+export default App;

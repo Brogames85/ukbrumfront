307e929c1883cfa84325596316ec52a57c4845ec
Mahir Handa <78787711+Brogames85@users.noreply.github.com>
2025-11-05 15:35:02 +0530

    Update app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,1 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>UKBRUM IS LIVE 🔥🔥</h1>
+      <p>Frontend deployed successfully!</p>
+    </div>
+  );
+}
+
+export default App;
 
